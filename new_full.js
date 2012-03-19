2f80a602f74214d7938380bdc8141451f08fb9e4
liwei <iloveiknow@sina.com>
2012-03-19 09:49:01 +0800

    add new_full

---
+++ new_full.js
@@ -0,0 +1,309 @@
+crystal.callbackarea([[{
+				"cid" : "1",
+				"oid" : "1",
+				"soid" : "675510AC00114F6687A219745000",
+				"aver" : "0",
+				"loc" : "News_takeover",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"resource_url" : "",
+						"link_to" : "",
+						"width" : 960,
+						"height" : 30,
+						"cover" : false
+					}
+				]
+			}
+		], [{
+				"cid" : "0",
+				"oid" : "100057720",
+				"soid" : "675510AC00114F6687A219745001",
+				"aver" : "1001",
+				"loc" : "News_Width1",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/adwin/1ece898d5814be6f8168d47daa033a632309.swf",
+						"link_to" : "http://c.l.qq.com/lclick?oid=100057720&loc=News_Width1&aver=1001&MCESS=MDAwMSZdPLkiyELia9x9pc0hWLM+5swHx+iPcWhA6rzyIjstIQ==&CEQPS=MDAwMSajzrYJRCZNJTd9mvvYPUpnu0thKq3swmMzMh7drRN7TxCGajO53/84g1GRLj0+L9s=&soid=675510AC00114F6687A219745001",
+						"width" : 960,
+						"height" : 90,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "250246",
+				"oid" : "1308382",
+				"soid" : "675510AC00114F6687A219745002",
+				"aver" : "0",
+				"loc" : "News_Full",
+				"display" : "fullscreen",
+				"display_config" : {
+					"delay" : 7,
+					"duration" : 7,
+					"show_duration" : .5,
+					"hide_duration" : .5
+				},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/201203/12/yqdzxs_NF_201203122910.jpg",
+						"link_to" : "http://c.l.qq.com/lclick?seq=20120312000275&loc=News_Full&soid=675510AC00114F6687A219745002",
+						"width" : 960,
+						"height" : 500,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "247749",
+				"oid" : "1304906",
+				"soid" : "675510AC00114F6687A219745003",
+				"aver" : "0",
+				"loc" : "News_Rectangle",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/201202/29/pw_NR_201202292334.jpg",
+						"link_to" : "http://c.l.qq.com/lclick?seq=20120229000046&loc=News_Rectangle&soid=675510AC00114F6687A219745003",
+						"width" : 250,
+						"height" : 230,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "0",
+				"oid" : "100055880",
+				"soid" : "675510AC00114F6687A219745004",
+				"aver" : "1001",
+				"loc" : "News_Button1",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/adwin/5eee6bcab6c7c8f2d1d0e231e658f3403448.swf",
+						"link_to" : "http://c.l.qq.com/lclick?oid=100055880&loc=News_Button1&aver=1001&MCESS=MDAwMSbceUFCrzXzPSjerFCqjEdJAJOu9rYxmMJVboMTDJjUyA==&CEQPS=MDAwMSajzrYJRCZNJTd9mvvYPUpnu0thKq3swmMzMh7drRN7TxCGajO53/84g1GRLj0+L9s=&soid=675510AC00114F6687A219745004",
+						"width" : 300,
+						"height" : 100,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "0",
+				"oid" : "100060285",
+				"soid" : "675510AC00114F6687A219745005",
+				"aver" : "1001",
+				"loc" : "News_Width2",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/adwin/46de4b2805360b6a0923c0021746fdcb3453.jpg",
+						"link_to" : "http://c.l.qq.com/lclick?oid=100060285&loc=News_Width2&aver=1001&MCESS=MDAwMSb/mpX3JyYIelhMxA5HfHRkFh+jjgarHfp9nQ8B359xWA==&CEQPS=MDAwMSajzrYJRCZNJTd9mvvYPUpnu0thKq3swmMzMh7drRN7TxCGajO53/84g1GRLj0+L9s=&soid=675510AC00114F6687A219745005",
+						"width" : 960,
+						"height" : 90,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "0",
+				"oid" : "100058828",
+				"soid" : "675510AC00114F6687A219745006",
+				"aver" : "1001",
+				"loc" : "News_Width3",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/adwin/c16e99b92732c3d63612665ce7d6d3a73445.jpg",
+						"link_to" : "http://c.l.qq.com/lclick?oid=100058828&loc=News_Width3&aver=1001&MCESS=MDAwMSZA6EA7t6UoNehA106++g5Ky9RmNSuR1mVYvJlo/HfZMg==&CEQPS=MDAwMSajzrYJRCZNJTd9mvvYPUpnu0thKq3swmMzMh7drRN7TxCGajO53/84g1GRLj0+L9s=&soid=675510AC00114F6687A219745006",
+						"width" : 960,
+						"height" : 90,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "246286",
+				"oid" : "1302995",
+				"soid" : "675510AC00114F6687A219745007",
+				"aver" : "0",
+				"loc" : "News_Rectangle2",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "http://v.admaster.com.cn/i/a10148,b200016703,c1351,i0,m202,h",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/201202/17/nmgyls_NR_201202175843.swf",
+						"link_to" : "http://c.l.qq.com/lclick?seq=20120217000195&loc=News_Rectangle2&soid=675510AC00114F6687A219745007",
+						"width" : 250,
+						"height" : 230,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "248388",
+				"oid" : "1305765",
+				"soid" : "675510AC00114F6687A219745008",
+				"aver" : "0",
+				"loc" : "News_Rectangle3",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/201203/02/zgdx_NR_201203020241.swf",
+						"link_to" : "http://c.l.qq.com/lclick?seq=20120302000018&loc=News_Rectangle3&soid=675510AC00114F6687A219745008",
+						"width" : 250,
+						"height" : 230,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "0",
+				"oid" : "100059416",
+				"soid" : "675510AC00114F6687A219745009",
+				"aver" : "1001",
+				"loc" : "News_Width4",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/adwin/d9cfe6e114334ada39ea7356ba18ea903454.jpg",
+						"link_to" : "http://c.l.qq.com/lclick?oid=100059416&loc=News_Width4&aver=1001&MCESS=MDAwMSZ1zXns9EAYm6/rmIeGOwyrYIogxXZGwOu+EczGv4T45w==&CEQPS=MDAwMSajzrYJRCZNJTd9mvvYPUpnu0thKq3swmMzMh7drRN7TxCGajO53/84g1GRLj0+L9s=&soid=675510AC00114F6687A219745009",
+						"width" : 960,
+						"height" : 90,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "0",
+				"oid" : "100059883",
+				"soid" : "675510AC00114F6687A219745010",
+				"aver" : "1001",
+				"loc" : "News_Width5",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/adwin/39ac4331469ca6d70dbe2942eb39c4763446.swf",
+						"link_to" : "http://c.l.qq.com/lclick?oid=100059883&loc=News_Width5&aver=1001&MCESS=MDAwMSa/hgw+0pbUj232DJooi6rNFdSigq85zLsle0N5rXv35w==&CEQPS=MDAwMSajzrYJRCZNJTd9mvvYPUpnu0thKq3swmMzMh7drRN7TxCGajO53/84g1GRLj0+L9s=&soid=675510AC00114F6687A219745010",
+						"width" : 960,
+						"height" : 90,
+						"cover" : true
+					}
+				]
+			}
+		], [], [], [{
+				"cid" : "245742",
+				"oid" : "1302347",
+				"soid" : "675510AC00114F6687A219745011",
+				"aver" : "0",
+				"loc" : "News_Rectangle4",
+				"display" : "banner",
+				"display_config" : {},
+				"monitor_url" : "http://ad-apac.doubleclick.net/imp;v1;f;252055204;0-0;0;76184567;1|1;46118220|46135348|1;;cs=g;%3fhttp://ad.cn.doubleclick.net/dot.gif?[timestamp]",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/201202/14/xays_NR_201202140736.swf",
+						"link_to" : "http://c.l.qq.com/lclick?seq=20120214000207&loc=News_Rectangle4&soid=675510AC00114F6687A219745011",
+						"width" : 250,
+						"height" : 230,
+						"cover" : true
+					}
+				]
+			}
+		], [{
+				"cid" : "1",
+				"oid" : "1",
+				"soid" : "675510AC00114F6687A219745012",
+				"aver" : "0",
+				"loc" : "News_BackPopWin",
+				"display" : "popup",
+				"display_config" : {
+					"delay" : 1,
+					"focus_back" : true
+				},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "",
+						"link_to" : "",
+						"width" : 750,
+						"height" : 500,
+						"cover" : false
+					}
+				]
+			}
+		], [{
+				"cid" : "1",
+				"oid" : "1",
+				"soid" : "675510AC00114F6687A219745013",
+				"aver" : "0",
+				"loc" : "News_RM_Width1",
+				"display" : "script_src",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"resource_url" : ""
+					}
+				]
+			}
+		], [{
+				"cid" : "251203",
+				"oid" : "1309678",
+				"soid" : "675510AC00114F6687A219745014",
+				"aver" : "0",
+				"loc" : "News_Couplet",
+				"display" : "couplet",
+				"display_config" : {},
+				"monitor_url" : "",
+				"fodder" : [{
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/201203/16/hr_NC_201203165528.jpg",
+						"link_to" : "http://c.l.qq.com/lclick?seq=20120316000156&loc=News_Couplet&soid=675510AC00114F6687A219745014",
+						"width" : 110,
+						"height" : 300,
+						"cover" : true
+					}, {
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/201203/16/hr_NC_201203165528.jpg",
+						"link_to" : "http://c.l.qq.com/lclick?seq=20120316000156&loc=News_Couplet&soid=675510AC00114F6687A219745014",
+						"width" : 110,
+						"height" : 300,
+						"cover" : true
+					}, {
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/201203/16/hr_NC_201203165542.jpg",
+						"link_to" : "http://c.l.qq.com/lclick?seq=20120316000156&loc=News_Couplet&soid=675510AC00114F6687A219745014",
+						"width" : 25,
+						"height" : 300,
+						"cover" : true
+					}, {
+						"type" : "",
+						"resource_url" : "http://adsfile.qq.com/201203/16/hr_NC_201203165542.jpg",
+						"link_to" : "http://c.l.qq.com/lclick?seq=20120316000156&loc=News_Couplet&soid=675510AC00114F6687A219745014",
+						"width" : 25,
+						"height" : 300,
+						"cover" : true
+					}
+				]
+			}
+		]])
